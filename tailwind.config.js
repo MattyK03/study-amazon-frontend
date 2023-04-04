aa92aeeef75968f998c4fb99b4941a7e4adb2868
Matty <mask0343@gmail.com>
2023-04-04 18:39:37 +0300

    project settings is ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,52 @@
 /** @type {import('tailwindcss').Config} */
+
+const twColors = require('tailwindcss/colors')
+
+const colors = {
+  transparent: twColors.transparent,
+  black: '#2E3239',
+  white: twColors.white,
+  secondary: '#FF9902',
+  primary: '#161D25',
+  'bg-color': '#F2F2F5',
+  aqua: '#268697',
+}
+
+
+
 module.exports = {
   content: [
-    "./app/**/*.{js, ts, jsx, tsx}",
+    "./src/**/*.{js, ts, jsx, tsx}",
     "./pages/**/*.{js, ts, jsx, tsx}",
     "./components/**/*.{js, ts, jsx, tsx}",
   ],
   theme: {
-    extend: {},
+    colors,
+    extend: {
+      keyframes: {
+        animationOpacity: {
+          from: { opacity: 0.2 },
+          to: { opacity: 1 }
+        },
+        scaleIn: {
+          '0%': {
+          opacity: 0,
+          tranform: 'scale(0.9)'
+          },
+          '50%': {
+            opacity: 0.3
+          },
+          '100%': {
+            opacity: 1,
+            tranform: 'scale(1)'
+          }
+        }
+      },
+      animation: {
+        opacity: 'animationOpacity .5s ease-in-out',
+        scaleIn: 'scaleIn .35s ease-in-out'
+      }
+    },
   },
   plugins: [],
 }
